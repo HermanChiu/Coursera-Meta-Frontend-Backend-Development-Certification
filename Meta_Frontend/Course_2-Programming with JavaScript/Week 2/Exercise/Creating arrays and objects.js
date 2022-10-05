61afbf2a239730935f4f7787f55a460cdfd7d3ff
Herman Chiu <herman4848@gmail.com>
2022-10-05 01:43:58 -0700

    Second exercise of Week 2

---
+++ Meta_Frontend/Course_2-Programming with JavaScript/Week 2/Exercise/Creating arrays and objects.js
@@ -0,0 +1,9 @@
+// Create a new empty array literal and assign it to the variable clothes.
+// Add 5 of your favorite items of clothing as strings using the push() method.
+// Remove the fifth piece of clothing from the array using the pop() method.
+// Add a new piece of clothing using the push() method.
+// Use console.log to show the third item from the clothes array in the console.
+// Create a new empty object literal and assign it to the variable favCar.
+// Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+// Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+// Use the console to log the entire favCar object.
